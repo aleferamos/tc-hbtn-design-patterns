b12c2fde88114f29983af3bebc31dd2efe0a469f
aleferamos <alefepdias@gmail.com>
2022-07-07 21:11:32 -0400

    t

--- 0x02/impressao/Impressao.java
+++ 0x02/impressao/Impressao.java
@@ -1,24 +1,50 @@
 public class Impressao {
 
-    int paginasTotais;
+    private int paginasTotais;
     int paginasColoridas;
     boolean ehFrenteVerso;
-    double valorColoridasFrenteVerso;
+    private double valorColoridasFrenteVerso;
     double valorPretoBrancoFrenteVerso;
 
-
     double valorColoridasFrenteApenas;
-    double valorPretoBrancoFrenteApenas;
+    private double valorPretoBrancoFrenteApenas;
 
-    double calcularTotal(){
-        return 0;
+    public void setPaginasTotais(int paginasTotais) {
+        this.paginasTotais = paginasTotais;
+    }
+
+    public void setPaginasColoridas(int paginasColoridas) {
+        this.paginasColoridas = paginasColoridas;
+    }
+
+    public void setEhFrenteVerso(boolean ehFrenteVerso) {
+        this.ehFrenteVerso = ehFrenteVerso;
+    }
+
+    public void setValorColoridasFrenteVerso(double valorColoridasFrenteVerso) {
+        this.valorColoridasFrenteVerso = valorColoridasFrenteVerso;
+    }
+
+    public void setValorPretoBrancoFrenteVerso(double valorPretoBrancoFrenteVerso) {
+        this.valorPretoBrancoFrenteVerso = valorPretoBrancoFrenteVerso;
+    }
+
+    public void setValorColoridasFrenteApenas(double valorColoridasFrenteApenas) {
+        this.valorColoridasFrenteApenas = valorColoridasFrenteApenas;
+    }
+
+    public void setValorPretoBrancoFrenteApenas(double valorPretoBrancoFrenteApenas) {
+        this.valorPretoBrancoFrenteApenas = valorPretoBrancoFrenteApenas;
+    }
+
+    public double calcularTotal(){
+        return valorColoridasFrenteVerso + valorPretoBrancoFrenteVerso + valorColoridasFrenteApenas +
+                        valorPretoBrancoFrenteApenas;
     }
 
     @Override
     public String toString() {
         return String.format("total de paginas: %d, total coloridas: %d, total preto e branco: %d, frente e verso. " +
-                "total: %.2f", paginasTotais, paginasColoridas, paginasTotais - paginasColoridas,
-                valorColoridasFrenteVerso + valorPretoBrancoFrenteVerso + valorColoridasFrenteApenas +
-                        valorPretoBrancoFrenteApenas);
+                "total: %.2f", paginasTotais, paginasColoridas, paginasTotais - paginasColoridas, calcularTotal());
     }
 }
--- 0x02/impressao/ImpressaoFactory.java
+++ 0x02/impressao/ImpressaoFactory.java
@@ -1,42 +1,42 @@
 public class ImpressaoFactory {
 
-    static Impressao createImpressao(TamanhoImpressao tamanho, int TotalPages, int QuantidadePaginasColoridas, boolean ehFrenteVerso){
+   public static Impressao createImpressao(TamanhoImpressao tamanho, int TotalPages, int QuantidadePaginasColoridas, boolean ehFrenteVerso){
         
         Impressao impressao = new Impressao();
         
         if(ehFrenteVerso){
             if(tamanho.equals(TamanhoImpressao.A2)){
                 impressao.paginasColoridas = QuantidadePaginasColoridas;
-                impressao.paginasTotais = TotalPages;
-                impressao.valorColoridasFrenteVerso = QuantidadePaginasColoridas * 0.28;
+                impressao.setPaginasTotais(TotalPages);
+                impressao.setValorColoridasFrenteVerso(QuantidadePaginasColoridas * 0.28);
                 impressao.valorPretoBrancoFrenteVerso = (TotalPages - QuantidadePaginasColoridas) * 0.18;
             } else if (tamanho.equals(TamanhoImpressao.A3)) {
                 impressao.paginasColoridas = QuantidadePaginasColoridas;
-                impressao.paginasTotais = TotalPages;
-                impressao.valorColoridasFrenteVerso = QuantidadePaginasColoridas * 0.25;
+                impressao.setPaginasTotais(TotalPages);
+                impressao.setValorColoridasFrenteVerso(QuantidadePaginasColoridas * 0.25);
                 impressao.valorPretoBrancoFrenteVerso = (TotalPages - QuantidadePaginasColoridas) * 0.15;
             } else if (tamanho.equals(TamanhoImpressao.A4)) {
                 impressao.paginasColoridas = QuantidadePaginasColoridas;
-                impressao.paginasTotais = TotalPages;
-                impressao.valorColoridasFrenteVerso = QuantidadePaginasColoridas * 0.20;
+                impressao.setPaginasTotais(TotalPages);
+                impressao.setValorColoridasFrenteVerso(QuantidadePaginasColoridas * 0.20);
                 impressao.valorPretoBrancoFrenteVerso = (TotalPages - QuantidadePaginasColoridas) * 0.10;
             }
         } else {
             if (tamanho.equals(TamanhoImpressao.A2)){
                 impressao.paginasColoridas = QuantidadePaginasColoridas;
-                impressao.paginasTotais = TotalPages;
+                impressao.setPaginasTotais(TotalPages);
                 impressao.valorColoridasFrenteApenas = QuantidadePaginasColoridas * 0.32;
-                impressao.valorPretoBrancoFrenteApenas = (TotalPages - QuantidadePaginasColoridas) * 0.22;
+                impressao.setValorPretoBrancoFrenteApenas((TotalPages - QuantidadePaginasColoridas) * 0.22);
             } else if (tamanho.equals(TamanhoImpressao.A3)) {
                 impressao.paginasColoridas = QuantidadePaginasColoridas;
-                impressao.paginasTotais = TotalPages;
+                impressao.setPaginasTotais(TotalPages);
                 impressao.valorColoridasFrenteApenas = QuantidadePaginasColoridas * 0.30;
-                impressao.valorPretoBrancoFrenteApenas = (TotalPages - QuantidadePaginasColoridas) * 0.20;
+                impressao.setValorPretoBrancoFrenteApenas((TotalPages - QuantidadePaginasColoridas) * 0.20);
             } else if (tamanho.equals(TamanhoImpressao.A4)) {
                 impressao.paginasColoridas = QuantidadePaginasColoridas;
-                impressao.paginasTotais = TotalPages;
+                impressao.setPaginasTotais(TotalPages);
                 impressao.valorColoridasFrenteApenas = QuantidadePaginasColoridas * 0.25;
-                impressao.valorPretoBrancoFrenteApenas = (TotalPages - QuantidadePaginasColoridas) * 0.15;
+                impressao.setValorPretoBrancoFrenteApenas((TotalPages - QuantidadePaginasColoridas) * 0.15);
             }
         }
 
