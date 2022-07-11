bc8547e1c8a0bbd15319bfbfda5f732cc450de86
aleferamos <alefepdias@gmail.com>
2022-07-11 11:43:05 -0400

    t

--- 0x03/midia_player/AudioPlayer.java
+++ 0x03/midia_player/AudioPlayer.java
@@ -2,16 +2,13 @@ public class AudioPlayer implements MediaPlayer {
 
     MediaPlayerAdapter mediaPlayerAdapter;
 
-    public AudioPlayer() {
-        this.mediaPlayerAdapter = new MediaPlayerAdapter();
-    }
-
     @Override
     public void reproduzir(String nomeArquivo, TipoMedia tipoMedia) {
         if(tipoMedia.equals(TipoMedia.MP3)){
             System.out.println("Reproduzindo MP3: " + nomeArquivo);
         } else {
-            mediaPlayerAdapter.reproduzir(nomeArquivo, tipoMedia);
+          this.mediaPlayerAdapter = new MediaPlayerAdapter(tipoMedia);
+          mediaPlayerAdapter.reproduzir(nomeArquivo, tipoMedia);
         }
 
     }
--- 0x03/midia_player/MediaPlayerAdapter.java
+++ 0x03/midia_player/MediaPlayerAdapter.java
@@ -2,8 +2,6 @@ public class MediaPlayerAdapter implements MediaPlayer {
 
     AdvancedMediaPlayer advancedMediaPlayer;
 
-    public MediaPlayerAdapter() {
-    }
 
     public MediaPlayerAdapter(TipoMedia tipoMedia) {
         if(!tipoMedia.equals(TipoMedia.MP3)){
@@ -13,6 +11,6 @@ public class MediaPlayerAdapter implements MediaPlayer {
 
     @Override
     public void reproduzir(String nomeArquivo, TipoMedia tipoMedia) {
-        System.out.println("Reproduzindo " + tipoMedia + ": " + nomeArquivo);
+        advancedMediaPlayer.reproduzirVlc(nomeArquivo);
     }
 }
