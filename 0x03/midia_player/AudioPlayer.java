63f4134e071732e35a7c1c658cf2ad87646c16fa
aleferamos <alefepdias@gmail.com>
2022-07-11 11:59:24 -0400

    t

--- 0x03/midia_player/AudioPlayer.java
+++ 0x03/midia_player/AudioPlayer.java
@@ -5,7 +5,7 @@ public class AudioPlayer implements MediaPlayer {
     @Override
     public void reproduzir(String nomeArquivo, TipoMedia tipoMedia) {
         if(tipoMedia.equals(TipoMedia.MP3)){
-            System.out.println("Reproduzindo MP3: " + nomeArquivo);
+            System.out.printf("Reproduzindo MP3: %s \n", nomeArquivo);
         } else {
           this.mediaPlayerAdapter = new MediaPlayerAdapter(tipoMedia);
           mediaPlayerAdapter.reproduzir(nomeArquivo, tipoMedia);
--- 0x03/midia_player/VideoMediaPlayer.java
+++ 0x03/midia_player/VideoMediaPlayer.java
@@ -2,11 +2,11 @@ public class VideoMediaPlayer implements AdvancedMediaPlayer {
 
     @Override
     public void reproduzirVlc(String nomeVideoVlc) {
-        System.out.println("Reproduzindo VLC: " + nomeVideoVlc);
+        System.out.printf("Reproduzindo VLC: %s", nomeVideoVlc);
     }
 
     @Override
     public void reproduzirMp4(String nomeVideoMP4) {
-        System.out.println("Reproduzindo MP4: " + nomeVideoMP4);
+        System.out.printf("Reproduzindo MP4: %s \n", nomeVideoMP4);
     }
 }
